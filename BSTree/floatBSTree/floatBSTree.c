6ce57f0c0ae01394d5983cd3ed6cee137808b99d
AdemMENHOUR <150479331+ademmenh@users.noreply.github.com>
2024-10-01 19:03:32 +0100

    new function
    funcfloatBSTreeDelete ()

--- BSTree/floatBSTree/floatBSTree.c
+++ BSTree/floatBSTree/floatBSTree.c
@@ -105,3 +105,92 @@ void funcfloatBSTreeInsert (floatBSTree *pBSTree, float value)
         funcfloatBSTreeInsert (&((*pBSTree)->LC), value);
     }
 }
+
+void funcfloatBSTreeDelete (floatBSTree *pBSTree, float value)
+{
+
+    if ( !funcfloatBSTreeEmpty(*pBSTree) )
+    {
+
+        if ( (*pBSTree)->Value == value )
+        {
+
+            if ( funcfloatBSTNodeLeaf (*pBSTree) )
+            {
+
+                floatBSTNode *vpTemp = (*pBSTree);
+                (*pBSTree) = NULL;
+                funcfloatBSTNodeFree (vpTemp);
+
+            }
+            else if ( (*pBSTree)->LC==NULL || (*pBSTree)->RC==NULL )
+            {
+
+                floatBSTNode *vpTemp;
+                if ((*pBSTree)->LC==NULL)
+                {
+                    vpTemp = (*pBSTree);
+                    (*pBSTree) = (*pBSTree)->RC;
+                    funcfloatBSTNodeFree (vpTemp);
+                }
+                else
+                {
+                    vpTemp = (*pBSTree);
+                    (*pBSTree) = (*pBSTree)->LC;
+                    funcfloatBSTNodeFree (vpTemp);
+                }
+
+            }
+            else
+            {
+                // searching for the max of mins
+
+                // in this case the BSTNode has two childs
+                // we use two pointers two pointers, one for swaping values and one for seting the BSTNode pointer
+                floatBSTNode *vpCnBefore, *vpCn;
+                vpCnBefore = (*pBSTree);
+                vpCn = (*pBSTree)->LC;
+
+                while ( vpCn->RC!=NULL )
+                {
+                    vpCnBefore = vpCn;
+                    vpCn = vpCn->RC;
+                }
+
+
+
+                // spwaping
+                (*pBSTree)->Value = vpCn->Value;
+                // handling the case where the vpCnBefore is the head of the tree
+                if ( vpCnBefore == (*pBSTree) )
+                {
+                    vpCnBefore->LC = vpCn->LC;
+                }
+                else
+                {
+                    vpCnBefore->RC = NULL;
+                }
+                // free space after permutation
+                funcfloatBSTNodeFree (vpCn);
+
+            }
+            
+            
+            return ;
+        }
+
+
+
+
+
+        if ( value < (*pBSTree)->Value )
+        {
+            funcfloatBSTreeDelete (&((*pBSTree)->LC), value);
+        }
+        else
+        {
+            funcfloatBSTreeDelete (&((*pBSTree)->RC), value);
+        }
+    }
+}
+
